90dbc9b4525b404a9fe7ac5ce64a0e7d3d9ab5ff
smitsrr <smitsrr@gmail.com>
2018-12-24 13:56:24 -0700

    love the progress

--- data_processing.R
+++ data_processing.R
@@ -1,10 +1,13 @@
 library(lubridate)  # date conversion
 library(sqldf)      # because i know what I want to do in SQL
 library(ggplot2)    # All plotting
+library(dplyr)      # data munging
 
 #pull in this handy data from a good person
 setwd ('..')
 lake<- read.csv('./dataisbeautiful-2018-12/data/lake_mendota.csv', stringsAsFactors = F)
+#save the file for me
+write.csv(lake, "lake_mendota_data.csv", row.names = F)
 
 # convert dates to dates
 lake$date_closed <- ymd(lake$date_closed)
@@ -27,7 +30,26 @@ lake_expanded<- sqldf("SELECT date
                       JOIN lake
                         ON all_dates.date >= lake.date_closed
                         AND all_dates.date <= lake.date_opened")
-lake_expanded$day <- format(lake_expanded$date, "%m-%d")
+# common year for plotting
+lake_expanded <- lake_expanded %>%
+  mutate(year2= case_when(
+    month(date) >= 10 ~ "2015",
+    TRUE ~ "2016")) %>%
+  mutate(x_axis_date = as.Date(paste0(year2, "-", format(lake_expanded$date, "%m-%d")), "%Y-%m-%d"))
+#to make the axis wrap, assign Oct 1- Dec 31 as 2015, and the rest to 2016
 
-qplot(lake_expanded$day)
+ggplot(lake_expanded, aes(x=x_axis_date, fill = as.factor(year))) +
+  geom_bar(stat = "count", position = "stack", width = 1) +
+  # scale_fill_continuous(low="blue", high="red")+ 
+  scale_x_date(date_labels = "%b") + 
+  geom_hline(yintercept=163) +
+  labs(y="", x = "", 
+       title = "Number of days Lake Mendota was open for ice fishing") +
+  theme_minimal() + 
+  theme(plot.title = element_text(size = 20, face = "bold", 
+                                  margin = margin(10,0,10,0)), 
+        axis.text = element_text(colour = "grey12"), 
+        axis.ticks = element_line(colour = "grey12"), 
+        plot.background = element_rect(colour = "grey82"), 
+        panel.background = element_rect(fill = "grey82", colour = NA)) 
 
